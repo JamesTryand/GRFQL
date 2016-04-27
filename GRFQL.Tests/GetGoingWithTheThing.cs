2d10eceade70512b56131e2210ecece1def44925
James Tryand <james.tryand@advancedcomputersoftware.com>
2016-04-27 16:46:16 +0100

    initial commit

---
+++ GRFQL.Tests/GetGoingWithTheThing.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GRFQL.Tests
+{
+    public class GetGoingWithTheThing
+    {
+        [Fact]
+        public void GetSchemaReturnsAString()
+        {
+            var sut = new GraphQLQuery<string, string>( (string x) => x as string);
+            var result = sut.GetSchema();
+            Assert.IsType<string>(result);
+        }
+
+        [Fact]
+        public void ANonNullSchemaIsCreatedWhenCreatedWithContent()
+        {
+            var sut = new GraphQLQuery<string,string>((string query) => query );
+            var result = sut.GetSchema();
+            Assert.IsType<string>(result);
+            Console.WriteLine(result);
+            Debug.WriteLine(result);
+            Assert.Equal("Thing",result);
+        }
+    }
+
+    public class GraphQLQuery<TIn, TOut>
+    {
+        private readonly Func<TIn, TOut> _func;
+
+        public GraphQLQuery(Func<TIn, TOut> func)
+        {
+            _func = func;
+        }
+
+        public string GetSchema()
+        {
+            return SerializeTypes(DetermineTypes(_func));
+        }
+
+        private IEnumerable<Type> DetermineTypes(Func<TIn, TOut> func)
+        {
+            yield return typeof (TIn);
+            yield return typeof (TOut);
+        }
+
+        private string SerializeTypes(IEnumerable<Type> determineTypes)
+        {
+            return ConcatDetails(determineTypes.Select(t => SerializeType(t)));
+        }
+        private string ConcatDetails(IEnumerable<string> @select)
+        {
+            return @select.Aggregate(new StringBuilder(), (sb, v) => sb.AppendLine(v), sb => sb.ToString());
+        }
+        private string SerializeType(Type type)
+        {
+            return type.ToString();
+        }
+    }
+}
---
+++ GRFQL.Tests/QueryParserTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GRFQL.Tests
+{
+    public class QueryParserTests
+    {
+        [Fact]
+        public void AnEmptyQueryReturnsNothing()
+        {
+            var QueryParser = new QueryParser(string.Empty);
+        }
+    }
+
+    public class QueryParser
+    {
+        private readonly string _query;
+
+        public QueryParser(string query)
+        {
+            _query = query;
+        }
+
+        public GQLHttpResponse Parse()
+        {
+            return new GQLHttpResponse(200, string.Empty);
+        }
+    }
+
+    public class GQLHttpResponse
+    {
+        public int HttpResponseCode { get; }
+        public string HttpResponse { get; }
+        public GQLHttpResponse(int httpResponseCode, string httpResponse)
+        {
+            HttpResponseCode = httpResponseCode;
+            HttpResponse = httpResponse;
+        }
+    }
+}
